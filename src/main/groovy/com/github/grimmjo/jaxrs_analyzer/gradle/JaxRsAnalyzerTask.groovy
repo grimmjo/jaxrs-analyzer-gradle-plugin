f6df6f63c32289182477e4a425824f5ba2210382
Luca <luca.santocono@xelion.com>
2020-09-14 12:26:56 +0200

    Upgrade to gradle 6.x wrapper and JDK 14.

--- src/main/groovy/com/github/grimmjo/jaxrs_analyzer/gradle/JaxRsAnalyzerTask.groovy
+++ src/main/groovy/com/github/grimmjo/jaxrs_analyzer/gradle/JaxRsAnalyzerTask.groovy
@@ -7,6 +7,8 @@ import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 
 import java.nio.file.Files
 import java.nio.file.Path
@@ -22,6 +24,7 @@ import java.util.stream.Stream
 class JaxRsAnalyzerTask extends DefaultTask {
 
     @InputDirectory
+    @PathSensitive(PathSensitivity.ABSOLUTE)
     File inputDirectory
 
     @OutputDirectory
